05e72499b0b9c1f58cf585987a6ddfecb55a826b
Movedizo <ramareid@hotmail.com>
2022-10-03 21:45:34 -0300

    acomodando card y carruseles

--- Selectores/js/index.js
+++ Selectores/js/index.js
@@ -16,13 +16,13 @@ function iniciarindex() {
 var imagenes = new Array();
  
 $(document).ready(function() {
-    var numImages = 6;
+    var numImages = 7;
     if (numImages <= 3) {
-        $('.right-arrow').css('display', 'none');
-        $('.left-arrow').css('display', 'none');
+        $('.flechader').css('display', 'none');
+        $('.flechaizq').css('display', 'none');
     }
  
-    $('.left-arrow').on('click',function() {
+    $('.flechaizq').on('click',function() {
         if (current > 0) {
             current = current - 1;
         } else {
@@ -34,27 +34,29 @@ $(document).ready(function() {
         return false;
     });
  
-    $('.left-arrow').on('hover', function() {
+    $('.flechaizq').on('hover', function() {
         $(this).css('opacity','0.5');
     }, function() {
         $(this).css('opacity','1');
     });
  
-    $('.right-arrow').on('hover', function() {
+    $('.flechader').on('hover', function() {
         $(this).css('opacity','0.5');
     }, function() {
         $(this).css('opacity','1');
     });
  
-    $('.right-arrow').on('click', function() {
+    $('.flechader').on('click', function() {
         if (numImages > current + 3) {
             current = current+1;
         } else {
             current = 0;
         }
  
-        $(".carrusel").animate({"left": -($('#product_'+current).position().left)}, 600);
- 
+        $(".carrusel").animate({"left": -($('.carsimple_'+current).position().left)}, 600);
+        $(".carrusel").animate({"left": -($('.cardoble_'+current).position().left)}, 600);
+        $(".carrusel").animate({"left": -($('.carmixto_'+current).position().left)}, 600);
+
         return false;
     }); 
  });*/
