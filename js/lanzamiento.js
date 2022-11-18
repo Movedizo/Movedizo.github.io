667e19dd91a5f6961ba2e39e244745fd3b48241e
franzodd <fran_tandil@hotmail.com>
2022-11-18 19:35:44 -0300

    menuDeplegable

---
+++ js/lanzamiento.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", iniciarindex);
+
+function iniciarindex() {
+    "use strict"
+    /* document.querySelector('#btnUsuario').addEventListener("click", function () {
+         document.querySelector('#menuHamburguesa').classList.add("desaparecer");
+         document.querySelector('#menuPerfil').classList.toggle("desaparecer");
+     });
+     
+     document.querySelector('#btnHamburguesa').addEventListener("click", function () {
+         document.querySelector('#menuPerfil').classList.add("desaparecer");
+         document.querySelector('#menuHamburguesa').classList.toggle("desaparecer");
+     });*/
+
+    let linksDesplegables = document.querySelectorAll(".linkDesplegable");
+    document.querySelector('#btnHamburguesa').addEventListener("click", traerMenu);
+
+
+    let num = 0;
+    let top = 114;
+
+    function traerMenu() {
+        console.log(num);
+        if (controlTraerMenu(linksDesplegables)) {
+            console.log("desaparecer");
+            if (num < linksDesplegables.length) {
+                linksDesplegables[num].classList.remove("aparecerMenu");
+                linksDesplegables[num].classList.add("desaparecerMenu");
+                linksDesplegables[num].style.top = top + "px";
+                setTimeout(traerMenu, 500);
+                top += 25;
+                num++;
+            }
+            else{
+                num = 0;
+                top = 114;
+            }
+        }
+        else if (!controlTraerMenu(linksDesplegables)) {
+            console.log("traer");
+            if (num < linksDesplegables.length) {
+                linksDesplegables[num].classList.remove("desaparecerMenu");
+                linksDesplegables[num].classList.add("aparecerMenu");
+                linksDesplegables[num].style.top = top + "px";
+                setTimeout(traerMenu, 500);
+                top += 25;
+                num++;
+            }
+            else{
+                num = 0;
+                top = 114;
+            }
+        }
+    }
+    function controlTraerMenu(lista) {
+        let boolean = false;
+        for (let index = 0; index < lista.length; index++) {
+            if (lista[index].classList.contains('aparecerMenu'))
+                boolean = true;
+            else
+                boolean = false;
+        }
+        return boolean;
+    }
+}
